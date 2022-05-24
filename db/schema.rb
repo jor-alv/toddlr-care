80095be47ce18772e64517a0870a34c8fbcb3d0c
Jorge Alvarez <jorge.a.alvarez@protonmail.com>
2022-05-24 15:14:45 -0400

    restores database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_190350) do
+ActiveRecord::Schema.define(version: 2022_05_24_190943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "consultations", force: :cascade do |t|
+    t.date "time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "daycares", force: :cascade do |t|
     t.string "address"
