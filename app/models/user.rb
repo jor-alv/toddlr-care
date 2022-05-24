350bd8cca2efa6a414b5a532ea4216b60517bf9b
Kim Coquilla <kimcoquilla@gmail.com>
2022-05-24 12:27:53 -0400

    Add column and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validate :first_last_company_name
+
+  def first_last_company_name
+    if first_name.present? && last_name.present?
+      validates :first_name, :last_name, presence: true
+      validates :company_name, absence: true
+    else
+      validates :company_name, presence: true
+      validates :first_name, :last_name, absence: true
+    end
+  end
 end
