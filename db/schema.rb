9d608b36277d158ec08409a001d6eaa7170efa5a
James Campbell <jcampbell92@icloud.com>
2022-05-31 10:11:17 -0400

    Added location column and ability for users to edit location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_234544) do
+ActiveRecord::Schema.define(version: 2022_05_31_140603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 2022_05_30_234544) do
     t.string "company_name"
     t.integer "category"
     t.text "bio"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
